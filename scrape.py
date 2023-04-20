73961cc18b5bd43797db176df7ed9ec2b7d9333a
carrotflowerr <64298096+carrotflowerr@users.noreply.github.com>
2023-04-20 22:41:28 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -10,15 +10,19 @@ user = str(input())
 driver = webdriver.Chrome('/sbin/chromedriver')
 driver.get('https://www.tiktok.com/'+str(user))
 
-time.sleep(5)
+
+# you can lower the time values if you have a faster system
+
+
+time.sleep(2)
 driver.refresh()
 
-time.sleep(5)
+time.sleep(2)
 last_height = driver.execute_script('return document.body.scrollHeight')
 
 while True:
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-    time.sleep(5)
+    time.sleep(2)
     new_height = driver.execute_script('return document.body.scrollHeight')
     
     if new_height == last_height:
@@ -26,8 +30,8 @@ while True:
     
     last_height = new_height
     
-    # Wait for 30 seconds to see if there's any additional videos
-    time.sleep(5)
+    # Wait for refresh
+    time.sleep(2)
     
 page_source = driver.page_source
 video_urls = re.findall(r'https://www.tiktok.com/'+str(user)+'/video/[^"]+', page_source)
